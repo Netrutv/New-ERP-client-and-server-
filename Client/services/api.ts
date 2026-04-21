721c317bfbb6e8956024c4ec5534e37b35669d5d
Netrutv <netrutvainternational@gmail.com>
2026-04-21 13:50:27 +0530

    Update api.ts

--- Client/services/api.ts
+++ Client/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // Replace with your current local IP
-export const BASE_URL = 'http://192.168.1.6:5000/api';
+//export const BASE_URL = 'http://192.168.1.6:5000/api';
 // export const BASE_URL = 'https://tracker-app-r6gy.onrender.com/api';
-// export const BASE_URL = 'https://erp-pro-1bic.onrender.com/api';
+ export const BASE_URL = 'https://erp-server-pzev.onrender.com/api';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
